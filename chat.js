d4e6cfc958a7fb680a1e4caae39b15e2047ba649
Shruti Pandey <48848864+gyalmo@users.noreply.github.com>
2021-05-26 11:24:50 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,52 @@
+const form = document.querySelector(".typing-area"),
+inputField = form.querySelector(".inputMsg"),
+sendBtn = form.querySelector("button"),
+chatBox = document.querySelector(".chat-box");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+}
+
+sendBtn.onclick = ()=>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/real-chat.php",true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                inputField.value = "";
+                scrollToBottom();
+            }
+        }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
+
+chatBox.onmouseenter = ()=>{
+    chatBox.classList.add("active");
+}
+chatBox.onmouseleave = ()=>{
+    chatBox.classList.remove("active");
+}
+
+setInterval(() =>{
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "php/fetch-chat.php",true);
+    xhr.onload = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            if(xhr.status === 200){
+                let data = xhr.response;
+                chatBox.innerHTML = data;
+                if(!chatBox.classList.contains("active")){
+                    scrollToBottom();
+                }
+            }
+        } 
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+  }, 500);
+
+  function scrollToBottom() {
+      chatBox.scrollTop = chatBox.scrollHeight;
+  }
